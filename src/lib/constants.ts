9236193048abb3ce551ddd2d24dbe01be252604b
Tim Martin <tim@timmart.in>
2023-06-13 15:03:15 -0400

    Fix TZ constant

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,6 @@
 // things should go here if they're used in multiple places
 // but, if they're secret, they should go in .env
 
-export const PATIENT_TIMEZONE = 'America/Chicago';
+export const PATIENT_TIMEZONE = 'America/New_York';
 export const CARE_EVENT_COLLECTION_NAME = 'care_events';
 export const DATETIME_INPUT_FORMAT = "yyyy-MM-dd'T'HH:mm";
